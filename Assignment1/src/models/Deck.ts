eaa393c0b7f25add7ba6754808e9b03325f97ceb
marwa93nezar <meme.sa93@gmail.com>
2024-09-15 18:05:48 +0200

    Change Deck to export it as function

--- Assignment1/src/models/Deck.ts
+++ Assignment1/src/models/Deck.ts
@@ -2,66 +2,59 @@ import { Card, Color, CardType } from './Card';
 
 export interface Deck {
   cards: Card[];
-  shuffle(): void;
-  drawCard(): Card | undefined;
-  dealCards(numCards: number): Card[];
-  resetDeck(): void;
 }
 
-export class UnoDeck implements Deck {
-  cards: Card[] = [];
+export const createDeck = (): Deck => {
+  const deck: Deck = { cards: [] };
+  resetDeck(deck);
+  shuffleDeck(deck);
+  return deck;
+};
 
-  constructor() {
-    this.resetDeck();
-    this.shuffle();
+export const shuffleDeck = (deck: Deck): void => {
+  for (let i = deck.cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck.cards[i], deck.cards[j]] = [deck.cards[j], deck.cards[i]];
   }
+};
 
-  shuffle(): void {
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+export const drawCard = (deck: Deck): Card | undefined => {
+  return deck.cards.pop();
+};
+
+
+export const dealCards = (deck: Deck, numCards: number): Card[] => {
+  const dealtCards: Card[] = [];
+  for (let i = 0; i < numCards; i++) {
+    const card = drawCard(deck);
+    if (card) dealtCards.push(card);
+  }
+  return dealtCards;
+};
+
+
+export const resetDeck = (deck: Deck): void => {
+  deck.cards = [];
+
+  const colors = [Color.Red, Color.Green, Color.Blue, Color.Yellow];
+
+  colors.forEach((color) => {
+    deck.cards.push({ color, type: CardType.Number, value: 0 });
+
+    for (let i = 1; i <= 9; i++) {
+      deck.cards.push({ color, type: CardType.Number, value: i });
+      deck.cards.push({ color, type: CardType.Number, value: i });
     }
-  }
 
-  drawCard(): Card | undefined {
-    return this.cards.pop();
-  }
-
-  dealCards(numCards: number): Card[] {
-    const dealtCards: Card[] = [];
-    for (let i = 0; i < numCards; i++) {
-      const card = this.drawCard();
-      if (card) dealtCards.push(card);
+    for (let i = 0; i < 2; i++) {
+      deck.cards.push({ color, type: CardType.Skip });
+      deck.cards.push({ color, type: CardType.Reverse });
+      deck.cards.push({ color, type: CardType.DrawTwo });
     }
-    return dealtCards;
+  });
+
+  for (let i = 0; i < 4; i++) {
+    deck.cards.push({ color: Color.Wild, type: CardType.Wild });
+    deck.cards.push({ color: Color.Wild, type: CardType.WildDrawFour });
   }
-
-  resetDeck(): void {
-    this.cards = [];
-
-    const colors = [Color.Red, Color.Green, Color.Blue, Color.Yellow];
-
-    colors.forEach((color) => {
-      this.cards.push({ color, type: CardType.Number, value: 0 });
-      for (let i = 1; i <= 9; i++) {
-        this.cards.push({ color, type: CardType.Number, value: i });
-        this.cards.push({ color, type: CardType.Number, value: i });
-      }
-      for (let i = 0; i < 2; i++) {
-        this.cards.push({ color, type: CardType.Skip });
-        this.cards.push({ color, type: CardType.Reverse });
-        this.cards.push({ color, type: CardType.DrawTwo });
-      }
-    });
-
-    for (let i = 0; i < 4; i++) {
-      this.cards.push({ color: Color.Wild, type: CardType.Wild });
-      this.cards.push({ color: Color.Wild, type: CardType.WildDrawFour });
-    }
-  }
-}
-
-const deck = new UnoDeck();
-console.log('Deck size:', deck.cards.length);
-const playerHand = deck.dealCards(7);
-console.log('Player hand:', playerHand);
+};
