9611683a399cce43b3dc745ebbed29c3e831f484
marwa93nezar <meme.sa93@gmail.com>
2024-09-27 19:15:13 +0200

    Game over component handle game restart

---
+++ Assignment2/uno/src/components/GameOver.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="game-over">
+      <h1>Game Over!</h1>
+      <p>Winner: {{ winner }}</p>
+      <button @click="restartGame">Play Again</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      winner() {
+        return this.$store.state.winner;
+      }
+    },
+    methods: {
+      restartGame() {
+        this.$store.dispatch('restartGame');
+        this.$router.push('/setup');
+      }
+    }
+  };
+  </script>
+  
