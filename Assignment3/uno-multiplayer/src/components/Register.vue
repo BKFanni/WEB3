c1923f6505c85ec5d0b7f06d5d651b967788bdb6
marwa93nezar <meme.sa93@gmail.com>
2024-10-08 21:56:09 +0200

    Add Register and game lobby components

---
+++ Assignment3/uno-multiplayer/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Register</h2>
+      <form @submit.prevent="register">
+        <input v-model="username" placeholder="Username" />
+        <input v-model="password" type="password" placeholder="Password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      async register() {
+        try {
+          await this.$store.dispatch('register', { username: this.username, password: this.password });
+          this.$router.push('/lobby');
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  };
+  </script>
+  
