a43a3dc49fc8707dacd0ce98041517dc40ccebb3
Andrejs Dāvis Umbrovskis <ledaviskylv@gmail.com>
2024-12-25 02:07:12 +0100

    Database type conversion to game types

--- Assignment6/uno/app/models/database/player.ts
+++ Assignment6/uno/app/models/database/player.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import { cardSchema } from "./card";
+import { cardSchema, convertToCard } from "./card";
+import { Player } from "../game/player";
+import { Card } from "../game/card";
 
 export const playerSchema = new mongoose.Schema({
     playerId: { // Player User Hex
@@ -20,3 +22,47 @@ export const playerSchema = new mongoose.Schema({
         default: false
     }
 });
+
+export function convertToPlayer(mongoosePlayer: unknown): Player {
+    // Basic checks
+    if (mongoosePlayer === null || mongoosePlayer === undefined || typeof mongoosePlayer !== "object")
+        throw(new Error("Invalid mongoose player schema type! (null/undefined/not object)"))
+
+    // Checking for necessary fields
+    if (!(
+        "playerId" in mongoosePlayer && "name" in mongoosePlayer && "hand" in mongoosePlayer
+        && "score" in mongoosePlayer && "calledUno" in mongoosePlayer
+    )) {
+        throw(new Error("Invalid mongoose player schema type! (doesn't have necessary fields!)"))
+    }
+
+    // Type checks
+    if (typeof mongoosePlayer.playerId !== "string")
+        throw(new Error("Invalid mongoose player schema type! (playerId isn't string!)"))
+
+    if (typeof mongoosePlayer.name !== "string")
+        throw(new Error("Invalid mongoose player schema type! (name isn't string!)"))
+
+    if (typeof mongoosePlayer.score !== "number")
+        throw(new Error("Invalid mongoose player schema type! (score isn't number!)"))
+
+    if (typeof mongoosePlayer.calledUno !== "boolean")
+        throw(new Error("Invalid mongoose player schema type! (calledUno isn't boolean!)"))
+
+    if (mongoosePlayer.hand === null || typeof mongoosePlayer.hand !== "object"
+        || !(mongoosePlayer.hand instanceof mongoose.Types.DocumentArray))
+        throw(new Error("Invalid mongoose player schema type! (hand isn't mongoose document array)"))
+
+    const handCards: Card[] = []
+    mongoosePlayer.hand.forEach(c => {
+        handCards.push(convertToCard(c))
+    });
+
+    return {
+        id: mongoosePlayer.playerId,
+        name: mongoosePlayer.name,
+        calledUno: mongoosePlayer.calledUno,
+        score: mongoosePlayer.score,
+        hand: handCards
+    }
+}
